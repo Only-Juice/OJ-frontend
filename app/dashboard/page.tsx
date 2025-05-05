adc090ad79813b7f3e6119231a64d22b9d200eb4
Joseph laptop <joseph122792@gmail.com>
2025-05-05 21:19:49 +0800

    use grid at little square

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,32 +4,6 @@ import Navbar from "@/components/Navbar";
 import { useEffect, useState } from "react";
 
 export default function Profile() {
-  const [avatarUrl, setAvatarUrl] = useState("");
-  const [username, setUsername] = useState("");
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch("https://ojapi.ruien.me/api/gitea/user", {
-          headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
-          },
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setAvatarUrl(data.data.avatar_url);
-          setUsername(data.data.login);
-        } else {
-          console.error("Failed to fetch user data");
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUser();
-  }, []);
-
   return (
     <div>
       <Navbar></Navbar>
@@ -48,21 +22,12 @@ export default function Profile() {
               })}
             </ul>
           </div>
-          {/* <div className="card-body gap-5">
-            <img src={avatarUrl} className="rounded-lg" />
-            <h2 className="card-title">{username}</h2>
-            {username}
-            <button className="btn btn-primary">Edit Profile</button>
-          </div> */}
         </div>
         <div className="flex-1 flex flex-col gap-10">
           <div className="flex-1 flex gap-10">
-            {/* <div className="card flex-1 bg-base-200">
-              <div className="card-body"></div>
-            </div> */}
             <div className="card flex-1 bg-base-200">
               <div className="card-body">
-                <div className="flex flex-wrap gap-4">
+                <div className="grid grid-cols-12 gap-4">
                   {Array.from({ length: 20 }).map((_, index) => {
                     const colors = ["#1CBABA", "#FFB700", "#F63737"];
                     const randomColor =
@@ -70,7 +35,7 @@ export default function Profile() {
                     return (
                       <div
                         key={index}
-                        className="w-12 h-12 flex  items-center justify-center text-white rounded"
+                        className="w-full aspect-square rounded"
                         style={{ backgroundColor: randomColor }}
                       ></div>
                     );
