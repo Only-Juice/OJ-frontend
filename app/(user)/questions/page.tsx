6c95cbccd548c3016da9ff601f22366efce2c096
Joseph home <joseph122792@gmail.com>
2025-08-18 00:47:34 +0800

    show Expired question opacity 50%

--- app/(user)/questions/page.tsx
+++ app/(user)/questions/page.tsx
@@ -38,31 +38,52 @@ export default function Problem() {
                 <th>Start Date</th>
                 <th>End Date</th>
                 <th>Status</th>
+                <th>Availability</th>
               </tr>
             )}
-            tbodyShow={(item, index) => (
-              <tr
-                key={index}
-                className="cursor-pointer hover:bg-base-200"
-                onClick={async () => {
-                  if (!item.has_question) {
-                    await takeQuestion(item.id);
-                  }
-                  router.push(`/questions/${item.id}`);
-                }}
-              >
-                <td>{item.title}</td>
-                <td>{toSystemDateFormat(new Date(item.start_time))}</td>
-                <td>{toSystemDateFormat(new Date(item.end_time))}</td>
-                <td>
-                  {item.top_score >= 100 ? (
-                    <CircleCheck className="text-green-500" />
+            tbodyShow={(item, index) => {
+              const isDisabled = new Date(item.end_time) < new Date();
+              return (
+                <tr
+                  key={index}
+                  className={`
+                    ${
+                      isDisabled
+                        ? "pointer-events-none opacity-50"
+                        : "cursor-pointer hover:bg-base-200"
+                    }`}
+                  onClick={async () => {
+                    if (isDisabled) return;
+                    if (!item.has_question) {
+                      await takeQuestion(item.id);
+                    }
+                    router.push(`/questions/${item.id}`);
+                  }}
+                >
+                  <td>{item.title}</td>
+                  <td>{toSystemDateFormat(new Date(item.start_time))}</td>
+                  <td>{toSystemDateFormat(new Date(item.end_time))}</td>
+                  <td>
+                    {item.top_score >= 100 ? (
+                      <CircleCheck className="text-green-500" />
+                    ) : (
+                      <CircleX className="text-red-500" />
+                    )}
+                  </td>
+                  {isDisabled ? (
+                    <td className="text-red-500 flex items-center gap-2">
+                      <CircleX />
+                      Expired
+                    </td>
                   ) : (
-                    <CircleX className="text-red-500" />
+                    <td className="text-green-500 flex items-center gap-2">
+                      <CircleCheck />
+                      Active
+                    </td>
                   )}
-                </td>
-              </tr>
-            )}
+                </tr>
+              );
+            }}
           />
         </div>
       </div>
