68857b14a413ad611eca7eb583f99a92a949dc6f
ZRE <chy853@gmail.com>
2025-09-11 03:32:42 +0800

    generate build info

---
+++ types/build-info.ts
@@ -0,0 +1,9 @@
+// Types for build information
+export interface BuildInfo {
+    commitHash: string;
+    shortCommitHash: string;
+    branchName: string;
+    commitDate: string;
+    buildTime: string;
+    buildTimestamp: number;
+}
